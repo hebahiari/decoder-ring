6c754da10ec35a0e19f0e2dfa52dbc08fe1c4040
Heba Hiari <hebahiary+github@gmail.com>
2021-11-06 08:04:11 -0700

    caeser code complete

--- src/caesar.js
+++ src/caesar.js
@@ -3,16 +3,47 @@
 // Only add code (e.g., helper methods, variables, etc.) within the scope
 // of the anonymous function on line 6
 
-const caesarModule = (function () {
-  // you can add any code you want within this function scope
+const caesarModule = (function() {
+    // you can add any code you want within this function scope
 
-  function caesar(input, shift, encode = true) {
-    // your solution code here
-  }
+    function caesar(input, shift, encode = true) {
+        if (shift == 0 || shift < -25 || shift > 25) return false;
 
-  return {
-    caesar,
-  };
+        if (!encode) shift = -shift;
+
+        let result = "";
+        input = input.toLowerCase();
+
+        for (let i = 0; i < input.length; i++) {
+            let charCode = input.charCodeAt(i);
+
+            if (charCode > 96 && charCode < 123) {
+                let shiftedCharCode = charCode + shift;
+
+                if (shiftedCharCode > 122) {
+                    shiftedCharCode = shiftedCharCode - 123 + 97;
+                }
+
+                if (shiftedCharCode < 97) {
+                    shiftedCharCode = 123 - (97 - shiftedCharCode);
+                }
+
+                let newLetter = String.fromCharCode(shiftedCharCode);
+                result += newLetter;
+            } else {
+                console.log(input[i]);
+                result += input[i];
+            }
+        }
+
+        console.log(result);
+
+        return result;
+    }
+
+    return {
+        caesar,
+    };
 })();
 
 module.exports = { caesar: caesarModule.caesar };
